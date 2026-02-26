87d5db1bbd4d588d599a963ff2cbceba8779803c
ferreirasara07 <ferreirasara5@icloud.com>
2026-02-26 10:28:58 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -12,6 +12,11 @@ em branco antes e depois da igualdade seguido pelo valor correspondente à soma
 de A e B.
 """
 
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
+# A = int(input())
+B = int(input())
+
+SOMA = A + B
+
+print(f"SOMA = {SOMA}")Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
 
 # Escreva sua solução abaixo
